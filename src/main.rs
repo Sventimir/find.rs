5494213d8356d97b6607c9241515d726534a62f7
Sventimir <svantimir@gmail.com>
2023-07-23 12:09:05 +0200

    Read the dir path from the command line.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::env;
 use std::fs;
 
 fn main() {
-  let paths = fs::read_dir(".").expect("Could not read directory");
+  let args : Vec<String> = env::args().collect();
+  let path = match &args.get(1) {
+    None => ".",
+    Some(path) => path,
+  };
+  let paths = fs::read_dir(path).expect("Could not read directory");
   for path in paths {
     println!("{}", path.unwrap().file_name().into_string().unwrap());
   }
